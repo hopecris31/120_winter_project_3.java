991e639fbf09299a37b90f66a65500754adde071
hopecris31 <hope.crisafi@gmail.com>
2022-02-20 21:21:23 -0500

    ugh

--- src/proj3/Deck.java
+++ src/proj3/Deck.java
@@ -1,6 +1,17 @@
 package proj3; // do not erase. Gradescope expects this.
 
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList contents;
+    public final int DEFAULT_DECK_SIZE = 52;
+
+    public Deck(){
+        contents = new ArrayList(DEFAULT_DECK_SIZE);
+
+
+    }
 
 
 }
