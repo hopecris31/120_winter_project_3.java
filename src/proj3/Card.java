b060a1da5064cc0bb8af8471647d819f5950229b
hopecris31 <hope.crisafi@gmail.com>
2022-02-20 20:34:28 -0500

    ugh

--- src/proj3/Card.java
+++ src/proj3/Card.java
@@ -18,11 +18,35 @@ public class Card {
         cardSuit = suit;
     }
 
+
     public getRank(){
         return cardRank;
     }
+
     public getSuit(){
         return cardSuit;
     }
 
+    /**
+     * take numeric rank and turn it into
+     * a printable string where 11-14 are
+     * turned into Face card values
+     */
+    private String toString(int rank){
+        if (rank == 11){
+            return "Jack";
+        }
+        else if (rank == 12) {
+            return "Queen";
+        }
+        else if (rank == 13) {
+            return "King";
+        }
+        else if (rank == 14) {
+            return "Ace";
+        }
+        else {  //whole card -- just change type
+            return "" + rank; //make int a string
+        }
+    }
 }
