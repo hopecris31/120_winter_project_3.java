75e97d712e1f6ff5f52551d6ddd93ecf9a0552b8
hopecris31 <hope.crisafi@gmail.com>
2022-02-20 21:59:19 -0500

    ugh

--- src/proj3/Deck.java
+++ src/proj3/Deck.java
@@ -6,6 +6,10 @@ public class Deck {
 
     private ArrayList contents;
     public final int DEFAULT_DECK_SIZE = 52;
+    public final int[] RANKS = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    public final String[] SUITS = {"Spades", "Hearts", "Diamonds", "Clubs"};
+
+
 
     public Deck(){
         contents = new ArrayList(DEFAULT_DECK_SIZE);
